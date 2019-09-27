b5a1469b191b590c4289b6b9bc9f00f84af97688
laurent naitza <lolonaitza@gmail.com>
2019-09-27 15:44:09 +0200

    Style Photo gallery et upload muli photo OK

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,18 @@
 class PhotosController < ApplicationController
   before_action :set_groupe, only: %i[show edit delete update sign_in new index create]
+  before_action :membre_verify, only: %i[show edit delete update sign_in new index create]
   skip_before_action :show_groupe_nav, only: %i[create update delete]
 
   def index
-    # byebug
+    if Membre.where(groupe_id: @groupe.id, user_id: current_user.id).count > 0
+    else
+      redirect_to sign_group_path(@groupe.id)
+    end
     if params[:query]
       @photos = Photo.where(groupe_id: @groupe.id).order(created_at: :asc)
     else
       @photos = Photo.where(groupe_id: @groupe.id).order(created_at: :desc)
     end
-
-
   end
 
   def new
@@ -47,6 +49,13 @@ class PhotosController < ApplicationController
 
   def set_groupe
     @groupe = Groupe.find(params[:groupe_id])
+  end
+
+  def membre_verify
+    if Membre.where(groupe_id: @groupe.id, user_id: current_user.id).count > 0
+    else
+      redirect_to sign_group_path(@groupe.id)
+    end
   end
 
 end
