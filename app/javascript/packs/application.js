6777c34f0931060bcfb525090e29e3a85c5fc658
laurent naitza <lolonaitza@gmail.com>
2019-10-22 14:01:27 +0200

    turbolink OP

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,53 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+
+console.log("vcoucou")
+
+window.addEventListener("turbolinks:load", (event) => {
+
+let nav_right_modal = document.getElementById('navRightModal');
+let nav_right_btn_open = document.getElementById('navRightBtn');
+let nav_right_btn_close = document.getElementById('navRightBtnClose');
+let nav_left_modal = document.getElementById('navLeftModal');
+let nav_left_btn_open = document.getElementById('navLeftBtn');
+
+if (document.body.offsetWidth <= 780 && nav_left_modal != null) {
+  if (nav_left_btn_open !=  null) {
+      nav_left_btn_open.children[0].classList.add("disabled");
+    }
+  }
+
+window.addEventListener('resize', function() {
+  if (document.body.offsetWidth <= 780 && nav_left_modal != null) {
+
+    nav_left_btn_open.children[0].classList.add("disabled");
+  } else if (document.body.offsetWidth > 780) {
+    nav_left_btn_open.children[0].classList.remove("disabled");
+  }
+});
+
+$(nav_right_btn_open).on("click", function(){
+  nav_right_modal.style.display = "block";
+})
+
+nav_right_btn_close.onclick = function() {
+  nav_right_modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == nav_right_modal) {
+    nav_right_modal.style.display = 'none';
+  }
+  else if (event.target == nav_left_modal) {
+    nav_left_modal.style.display = 'none';
+  }
+}
+
+if (nav_left_btn_open !=  null) {
+  nav_left_btn_open.onclick = function() {
+     nav_left_modal.style.display = "block";
+   }
+ }
+
+});
